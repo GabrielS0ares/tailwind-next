b3a74f4027bf9e20676a0a4f3c77656abd1aca73
GabrielS0ares <soaresgabriel33@gmail.com>
2023-11-18 21:07:16 -0300

    first commit

---
+++ src/app/components/BotaoManeiro.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { RotateCw } from 'lucide-react'
+import { useState } from 'react'
+
+export function BotaoManeiro() {
+  const [loading, setLoading] = useState(false)
+
+  async function ExecutaAlgo() {
+    setLoading(true)
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+    setLoading(false)
+  }
+
+  return (
+    <section className="flex h-screen items-center justify-center bg-neutral-700">
+      {/* Alterar disabled do botão muda o estilo dele com o enable e o disabled */}
+      <button
+        disabled={!!loading}
+        className="mt-4 w-36 rounded-md bg-sky-600/80 px-4 py-2 font-medium text-white duration-200 enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:bg-sky-800"
+        onClick={() => ExecutaAlgo()}
+      >
+        {!loading ? (
+          'Botão Maneiro'
+        ) : (
+          <div className="flex items-center gap-2">
+            <p>Carregando</p>
+            <div>
+              <RotateCw size={20} className="animate-spin text-sky-300" />
+            </div>
+          </div>
+        )}
+      </button>
+    </section>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { BotaoManeiro } from './components/BotaoManeiro'
+
+export default function Home() {
+  return (
+    <main>
+      <BotaoManeiro />
+    </main>
+  )
+}
