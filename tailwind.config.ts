5322790c0e6bd9eedcd8ae7d2c0f75ed9bc4e5bf
GabrielS0ares <soaresgabriel33@gmail.com>
2023-11-22 20:55:03 -0300

    Feat: Animando abertura dos Selects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,15 @@ const config: Config = {
           25: '#fcfaff',
         },
       },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translatey(-2px)' },
+          to: { opacity: '1', transform: 'translatey(0)' },
+        },
+      },
+      animation: {
+        slideDownAndFade: 'slideDownAndFade .2s cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [],
